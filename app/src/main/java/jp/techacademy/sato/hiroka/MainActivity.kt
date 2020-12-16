51c52ef6bf0e2cee3b7675a67e529beff8f3d724
佐藤宙花 <hiroka_sato@mjs.co.jp>
2020-12-16 23:10:22 +0900

    Android2演習

---
+++ app/src/main/java/jp/techacademy/sato/hiroka/MainActivity.kt
@@ -0,0 +1,11 @@
+package jp.techacademy.sato.hiroka
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
